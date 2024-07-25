a779a1520ec73246f6350e6922bcfb9a509fb1e3
Loren Q <lorenquiceno@gmail.com>
2024-07-24 20:59:14 -0500

    feat: ejercicio angular perfil-videojuegos

---
+++ angular-projects/perfil-videojuegos/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+
+@NgModule({
+  declarations: [AppComponent, PerfilComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
